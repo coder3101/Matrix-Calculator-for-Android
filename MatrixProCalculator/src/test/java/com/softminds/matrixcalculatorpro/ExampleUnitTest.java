1c7de0f0c908094ecde54b406e0726839b65ef09
coder3101 <ashar786khan@gmail.com>
2017-02-23 21:27:36 +0530

    test of logical functions

--- MatrixProCalculator/src/test/java/com/softminds/matrixcalculatorpro/ExampleUnitTest.java
+++ MatrixProCalculator/src/test/java/com/softminds/matrixcalculatorpro/ExampleUnitTest.java
@@ -10,8 +10,19 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void RowMultiples() throws Exception{
+        Matrix matrix = new Matrix();
+        matrix.SetCol(3);
+        matrix.SetRow(3);
+        for(int i=0;i<3;i++)
+            for(int j=0;j<3;j++)
+                matrix.SetElementof(i*10+j,i,j);
+        matrix.SetElementof(0,0,2);
+        matrix.SetElementof(10,1,2);
+        matrix.SetElementof(20,2,2);
+        System.out.println(matrix.toString());
+        assertTrue(matrix.RowEqualModulo());
     }
 }
