b551584eb8ff082bfbe1748a768032b4ee0796a4
coder3101 <ashar786khan@gmail.com>
2017-05-23 10:49:42 +0530

    Touch listeners to FAQ Cards and Color Changed

--- MatrixCalculator/src/main/java/com/softminds/matrixcalculator/base_activities/faqs.java
+++ MatrixCalculator/src/main/java/com/softminds/matrixcalculator/base_activities/faqs.java
@@ -20,14 +20,18 @@
 
 package com.softminds.matrixcalculator.base_activities;
 
+import android.animation.ObjectAnimator;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -36,7 +40,7 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.softminds.matrixcalculator.R;
 
-public class faqs extends AppCompatActivity {
+public class faqs extends AppCompatActivity implements View.OnTouchListener {
 
     boolean isDark;
     @Override
@@ -49,6 +53,8 @@ public class faqs extends AppCompatActivity {
             setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help_layout);
+
+        SetOnMotionEventToAll();
 
         if(!((GlobalValues)getApplication()).DonationKeyFound()) {
             AdView faqAd = (AdView) findViewById(R.id.adViewFaq);
@@ -66,16 +72,14 @@ public class faqs extends AppCompatActivity {
             SetThisColorToCard(ContextCompat.getColor(this,R.color.DarkcolorPrimary));
             SetThisColorToAllQuestions(ContextCompat.getColor(this,R.color.DarkcolorAccent));
         }
-        else
-        {
-            SetThisColorToCard(ContextCompat.getColor(this,R.color.cardColor));
+        else {
             SetThisColorToAllQuestions(ContextCompat.getColor(this,R.color.colorAccent));
         }
 
     }
     private void SetThisColorToCard(int id)
     {
-        //grab all 10 cards
+        //grab all 7 cards
         CardView cardView1 = (CardView) findViewById(R.id.QA1);
         CardView cardView2 = (CardView) findViewById(R.id.QA2);
         CardView cardView3 = (CardView) findViewById(R.id.QA3);
@@ -110,5 +114,44 @@ public class faqs extends AppCompatActivity {
         textView8.setTextColor(id);
         textView9.setTextColor(id);
         textView10.setTextColor(id);
+    }
+
+    private void SetOnMotionEventToAll(){
+        final CardView cardView1 = (CardView) findViewById(R.id.QA1);
+        CardView cardView2 = (CardView) findViewById(R.id.QA2);
+        CardView cardView3 = (CardView) findViewById(R.id.QA3);
+        CardView cardView5 = (CardView) findViewById(R.id.QA5);
+        CardView cardView8 = (CardView) findViewById(R.id.QA8);
+        CardView cardView9 = (CardView) findViewById(R.id.QA9);
+        CardView cardView10 = (CardView) findViewById(R.id.QA10);
+
+        cardView1.setOnTouchListener(this);
+        cardView3.setOnTouchListener(this);
+        cardView2.setOnTouchListener(this);
+        cardView5.setOnTouchListener(this);
+        cardView8.setOnTouchListener(this);
+        cardView9.setOnTouchListener(this);
+        cardView10.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                ObjectAnimator animator = ObjectAnimator.ofFloat(view,"translationZ",20);
+                animator.setDuration(200);
+                animator.setInterpolator(new DecelerateInterpolator());
+                animator.start();
+                return true;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP:
+                ObjectAnimator animator2 = ObjectAnimator.ofFloat(view,"translationZ",0);
+                animator2.setDuration(200);
+                animator2.setInterpolator(new AccelerateInterpolator());
+                animator2.start();
+                return true;
+            default:
+                return false;
+        }
     }
 }
